8e469bab66224974ec9b23034171773453c4437e
JakeRosa <joaquimvr15@gmail.com>
2024-05-15 15:00:51 +0100

    hotfix to run sonarcloud

--- myvet/src/main/java/tqs/myvet/repositories/AppointmentRepository.java
+++ myvet/src/main/java/tqs/myvet/repositories/AppointmentRepository.java
@@ -1,14 +1,18 @@
 package tqs.myvet.repositories;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 import tqs.myvet.entities.Appointment;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    List<Appointment> findByDate(String date);
+    List<Appointment> findByDate(Date date);
+
     List<Appointment> findByHour(String hour);
+
     List<Appointment> findByType(String type);
 }
