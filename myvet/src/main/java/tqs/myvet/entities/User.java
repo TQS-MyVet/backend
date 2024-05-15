e2de620f0490ca2ce5cb5a8256179814737dded8
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2024-05-15 16:52:43 +0100

    MYVET-28 Update

--- myvet/src/main/java/tqs/myvet/entities/User.java
+++ myvet/src/main/java/tqs/myvet/entities/User.java
@@ -1,12 +1,12 @@
 package tqs.myvet.entities;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,33 +14,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "pet")
-public class Pet {
+@Table(name = "user")
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
     @Size(min = 2, max = 30)
-    private String petName;
-
-    @NotNull
-    private String petSex;
-
-    @NotNull
-    private String petBirthdate;
+    private String name;
 
     @NotNull
     @Size(min = 2, max = 30)
-    private String petSpecies;
+    private String email;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @NotNull
+    private int phone;
+
+    @NotNull
+    private String password;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Pet> pets;
 
 }
---
+++ myvet/src/main/java/tqs/myvet/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package tqs.myvet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+import tqs.myvet.entities.Pet;
+
+@Repository
+public interface UserRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findPetsByUserId(Long id);
+
+}
