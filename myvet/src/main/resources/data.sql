2c8e9a0913c8b284037578257f002c1962124541
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-06-03 00:20:38 +0100

    small fix

--- myvet/src/main/resources/data.sql
+++ myvet/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- Password is "password" --
-INSERT INTO users (id, email, name, password, phone, roles) VALUES 
+INSERT INTO users (id, email, name, password, phone, roles) VALUES
 (1, 'everything@email.com', 'John Doe', '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{DOCTOR, RECEPTIONIST, USER}'),
-(2, 'doctor@email.com', "Doctor Doe", '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{DOCTOR, USER}'),
-(3, 'receptionist@email.com', "Receptionist Doe", '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{RECEPTIONIST, USER}'),
-(4, 'user@email.com', "User Doe", '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{USER}'),
+(2, 'doctor@email.com', 'Doctor Doe', '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{DOCTOR, USER}'),
+(3, 'receptionist@email.com','Receptionist Doe', '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{RECEPTIONIST, USER}'),
+(4, 'user@email.com', 'User Doe', '{bcrypt}$2a$10$kmymcENSj6rd2mHHbhpNR.TXypxzKgFGFCMuAlvTXpQ4grvUQvf6C', '1234567890', '{USER}');
